25ee0ebd4e6b7b829b1f66873bbff158c7640604
Steven Tappert <admin@dark-it.net>
2015-08-09 03:38:23 +0200

    added acpi-action support

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ I've tried to rebuild the auto-mode from osx
 # Install
 - Simply start ;-)
 
+# ACPI Support
+To activate ACPI support edit the ```/etc/acpi/handler.sh``` and add the matching actions
+
+## ACPI Actions
+- lidopen
+- lidclose
+
+## Example
+```bash
+   button/lid)
+        case "$3" in
+            close)
+                logger 'LID closed'
+                xset -display :0 dpms force off
+                /home/sammy8806/work/ebsk/ebsk.py --acpi=lidclose
+                ;;
+            open)
+                logger 'LID opened'
+                /home/sammy8806/work/ebsk/ebsk.py --acpi=lidopen
+                ;;
+    esac
+    ;;
+```
+
 # License
 (c) 2015 Steven Tappert
 Licensed under the MIT/X Consortium License.
--- ebsk.py
+++ ebsk.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
 import time
 import math
+import sys
+import getopt
 from threading import Thread
 
 DEBUG=False
@@ -23,7 +25,13 @@ class Sensor:
         file.close()
 
     def _read(self, file, name):
-        file = self._open(name, 'r')
+        try:
+            file = self._open(name, 'r')
+        except FileNotFoundError:
+            if DEBUG:
+                print('File %s was not found' % name)
+            return False
+
         value = file.readline()
         self._close(file)
         if DEBUG:
@@ -53,6 +61,31 @@ class LightSensor(Sensor):
         self.filename = '/sys/devices/platform/applesmc.768/light'
 
 class LightActor(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.forceOff = False
+
+    def writeForceOff(self, state=True):
+        name = '/tmp/ebsk_force_off_' + self.filename.replace('/', '_')
+        if state == True or state == 1:
+            self._write(name, 1)
+        else:
+            self._write(name, 0)
+
+    def checkForceOff(self):
+        file = None
+        name = '/tmp/ebsk_force_off_' + self.filename.replace('/','_')
+        value = self._read(file, name)
+        if value is False or value == "0":
+            self.forceOff = False
+            if DEBUG:
+                print("[%s] Force OFF ===> NO" % name)
+        elif value == "1":
+            self.forceOff = True
+            if DEBUG:
+                print("[%s] Force OFF ===> YES" % name)
+        return self.forceOff
+
     def readMax(self, file=None):
         if file is None:
             file = self.filename.replace('brightness', 'max_brightness')
@@ -60,6 +93,9 @@ class LightActor(Actor):
         return self.maxValue
 
     def write(self, value):
+        if self.checkForceOff() is True:
+           value = 0
+
         if value <= self.maxValue:
             return Actor.write(self, value)
         else:
@@ -89,46 +125,81 @@ class KeyboardBacklight(LightActor):
         LightActor.__init__(self)
         self.filename = '/sys/class/leds/smc::kbd_backlight/brightness'
         self.readMax()
+        self.maxValue = int(self.maxValue * 0.25);
 
 ambient = LightSensor()
 backlight = Backlight()
 kb_backlight = KeyboardBacklight()
 
+def trigger_action(type, args):
+    if type == "acpi":
+        if args == "lidclose":
+            backlight.writeForceOff()
+            kb_backlight.writeForceOff()
+        elif args == "lidopen":
+            backlight.writeForceOff(False)
+            kb_backlight.writeForceOff(False)
+
 def thread_bl():
+    print('Starting Backlight Thread')
     while True:
         backlight.fade(
-            math.ceil((backlight.maxValue / 255) * (int(ambient.read().split(',')[0][1:]) + 22)),
+            math.ceil((backlight.maxValue / 255) * (int(ambient.read().split(',')[0][1:]) + 12)),
             math.ceil(backlight.maxValue/(1024/3))
         )
         time.sleep(0.3)
 
 def thread_kbbl():
+    print('Starting Keyboard-Backlight Thread')
     while True:
-        kb_backlight.fade(
-            math.ceil((kb_backlight.maxValue / 255) * (255 - int(ambient.read().split(',')[0][1:]))),
-            math.ceil(kb_backlight.maxValue / 16)
-        )
+        # an extra if here because the ambient don't need to be read if it's forceOff
+        if kb_backlight.forceOff == False:
+            kb_backlight.fade(
+                math.ceil((kb_backlight.maxValue / 255) * (255 - int(ambient.read().split(',')[0][1:]))),
+                math.ceil(kb_backlight.maxValue / 16)
+            )
+        else:
+            kb_backlight.checkForceOff()
         time.sleep(0.3)
 
-t_bl = Thread(target=thread_bl)
-t_kbbl = Thread(target=thread_kbbl)
+def main(argv):
+   try:
+      opts, args = getopt.getopt(argv,"a::",["acpi="])
+   except getopt.GetoptError:
+      print('test.py --acpi=<acpi-action>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py --acpi=<acpi-action>')
+         sys.exit()
+      elif opt in ("--acpi"):
+         trigger_action(type="acpi", args=arg)
+   if len(opts) == 0:
+      t_bl = Thread(target=thread_bl)
+      t_kbbl = Thread(target=thread_kbbl)
 
-t_bl.start()
-t_kbbl.start()
+      t_bl.start()
+      t_kbbl.start()
 
-while True:
+      while True:
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+# Some useless code here
+#while True:
     #print((int(ambient.read().split(',')[0][1:]) + 10))
     #print(backlight.maxValue / 255)
     #print((backlight.maxValue / 255) * (int(ambient.read().split(',')[0][1:]) + 10))
-    time.sleep(10)
+#    time.sleep(10)
 
-for x in range(0,backlight.maxValue, math.ceil(backlight.maxValue/1024)):
-    backlight.write(x)
-    print("Setting BL to %s" % str(x) )
-    time.sleep(0.01)
+#for x in range(0,backlight.maxValue, math.ceil(backlight.maxValue/1024)):
+#    backlight.write(x)
+#    print("Setting BL to %s" % str(x) )
+#    time.sleep(0.01)
 
-
-for x in range(0,kb_backlight.maxValue, math.ceil(backlight.maxValue/32)):
-    kb_backlight.write(x)
-    print("Setting KBBL to %s" % str(x) )
-    time.sleep(0.05)
+#for x in range(0,kb_backlight.maxValue, math.ceil(backlight.maxValue/32)):
+#    kb_backlight.write(x)
+#    print("Setting KBBL to %s" % str(x) )
+#    time.sleep(0.05)
